bf6e9fe4de26a3afdf7016809c24d46628710880
aledguedes <alexandredguedes@gmail.com>
2021-09-23 21:15:49 -0300

    created dependeces JPA class

--- src/main/java/com/aledguedes/helpdesk/helpdesk/domain/Pessoa.java
+++ src/main/java/com/aledguedes/helpdesk/helpdesk/domain/Pessoa.java
@@ -5,17 +5,38 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.aledguedes.helpdesk.helpdesk.enuns.Perfil;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import com.aledguedes.helpdesk.helpdesk.enuns.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
 public abstract class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String nome;
+	
+	@Column(unique = true)
 	protected String cpf;
+	
+	@Column(unique = true)
 	protected String email;
 	protected String senha;
 
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "perfis")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 
 	public Pessoa() {
--- src/main/java/com/aledguedes/helpdesk/helpdesk/domain/Tecnico.java
+++ src/main/java/com/aledguedes/helpdesk/helpdesk/domain/Tecnico.java
@@ -3,18 +3,25 @@ package com.aledguedes.helpdesk.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.aledguedes.helpdesk.helpdesk.enuns.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa {
 
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
